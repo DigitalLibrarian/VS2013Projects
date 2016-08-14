dafcfbf168eea10c58da6683db2cc580407665bb
DigitalLibrarian <likwid38225@gmail.com>
2016-08-14 13:14:54 -0500

    Type file spread out

---
+++ Tiles/DriverConsole/Tiles/Items/ItemFactory.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using Tiles.Agents;
+using Tiles.Agents.Combat;
+using Tiles.Bodies;
+using Tiles.Math;
+
+namespace Tiles.Items
+{
+    public class ItemFactory : IItemFactory
+    {
+        public IItem Create(IItemClass itemClass)
+        {
+            return new Item
+            {
+                Class = itemClass
+            };
+        }
+
+        public IItem CreateShedLimb(IAgent agent, IBodyPart part)
+        {
+            return Create(new ItemClass
+            {
+                Name = string.Format("{0}'s {1}", agent.Name, part.Name),
+                Sprite = new Sprite(Symbol.CorpseBodyPart, Color.DarkGray, Color.Black),
+                WeaponClass = DefaultWeaponClass
+            });
+        }
+
+        private static IWeaponClass DefaultWeaponClass = new WeaponClass(
+                    name: "Strike",
+                    sprite: null,
+                    slots: new WeaponSlot[] { WeaponSlot.Main },
+                    attackMoveClasses: new ICombatMoveClass[] { 
+                           new CombatMoveClass(
+                               name: "Strike",
+                               meleeVerb: new Verb(
+                               new Dictionary<VerbConjugation, string>()
+                               {
+                                   { VerbConjugation.FirstPerson, "strike"},
+                                   { VerbConjugation.SecondPerson, "strike"},
+                                   { VerbConjugation.ThirdPerson, "strikes"},
+                               }, true),
+                               damage: new DamageVector(
+                                        new Dictionary<DamageType,uint>{
+                                            { DamageType.Slash, 1 },
+                                            { DamageType.Pierce, 1 },
+                                            { DamageType.Blunt, 1 },
+                                            { DamageType.Burn, 1 }
+                                        }
+                                   )
+                               )
+                           {
+                               IsMartialArts = true,
+                               IsDefenderPartSpecific = true,
+                               IsItem = true,
+                               IsStrike = true
+                           },
+
+                    });
+
+
+        public IItem CreateCorpse(IAgent agent)
+        {
+            return Create(new ItemClass
+            {
+                Name = string.Format("{0}'s corpse", agent.Name),
+                Sprite = new Sprite(Symbol.Corpse, Color.DarkGray, Color.Black)
+            });
+        }
+    }
+}
