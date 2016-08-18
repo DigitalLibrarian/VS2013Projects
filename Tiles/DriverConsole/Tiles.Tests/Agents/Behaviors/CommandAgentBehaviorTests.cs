404b52cdd706e648dc535345061e169baa94d993
DigitalLibrarian <likwid38225@gmail.com>
2016-08-18 13:24:24 -0500

    Added missing multiple command planning tests

--- Tiles/DriverConsole/Tiles.Tests/Agents/Behaviors/CommandAgentBehaviorTests.cs
+++ Tiles/DriverConsole/Tiles.Tests/Agents/Behaviors/CommandAgentBehaviorTests.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Moq;
 using Tiles.Agents.Behaviors;
 using Tiles.Agents;
+using Tiles.Tests.Assertions;
 
 namespace Tiles.Tests.Agents.Behaviors
 {
@@ -251,12 +252,63 @@ namespace Tiles.Tests.Agents.Behaviors
             ContextMock.Verify(x => x.Execute(gameMock.Object, AgentMock.Object, pump2Time), Times.Once());
         }
 
-        [Ignore]
         [TestMethod]
-        public void PlannerFailsToPlan() { }
+        public void PlannerFailsToPlan() 
+        {
+            var gameMock = new Mock<IGame>();
+            gameMock.Setup(x => x.DesiredFrameLength).Returns(1);
+            ContextMock.Setup(x => x.HasCommand).Returns(false);
 
-        [Ignore]
+            Asserter.AssertException<InvalidOperationException>(() =>
+            {
+                Behavior.Update(gameMock.Object, AgentMock.Object);
+            });
+
+            PlannerMock.Verify(x => x.PlanBehavior(gameMock.Object, AgentMock.Object), Times.Once());
+        }
+
         [TestMethod]
-        public void PlannerPlansMultipleCommands() { }
+        public void PlannerPlansMultipleCommands()
+        {
+            var gameMock = new Mock<IGame>();
+            gameMock.Setup(x => x.DesiredFrameLength).Returns(1);
+            ContextMock.Setup(x => x.HasCommand).Returns(false);
+
+            var commandMock1 = new Mock<IAgentCommand>();
+            commandMock1.Setup(x => x.RequiredTime).Returns(2);
+            var commandMock2 = new Mock<IAgentCommand>();
+            commandMock2.Setup(x => x.RequiredTime).Returns(1);
+            var commandMock3 = new Mock<IAgentCommand>();
+            commandMock3.Setup(x => x.RequiredTime).Returns(1);
+            var commands = new List<IAgentCommand>{
+                commandMock1.Object,
+                commandMock2.Object,
+                commandMock3.Object
+            };
+            PlannerMock.Setup(x => x.PlanBehavior(gameMock.Object, AgentMock.Object)).Returns(commands);
+
+            ContextMock.Setup(x => x.Execute(gameMock.Object, AgentMock.Object, 1)).Returns(1);
+            Behavior.Update(gameMock.Object, AgentMock.Object);
+
+            PlannerMock.Verify(x => x.PlanBehavior(gameMock.Object, AgentMock.Object), Times.Once());
+
+            CommandQueueMock.Verify(x => x.Next(), Times.Once());
+            ContextMock.Verify(x => x.StartNewCommand(gameMock.Object, commandMock1.Object), Times.Once());
+            ContextMock.Verify(x => x.StartNewCommand(gameMock.Object, It.IsAny<IAgentCommand>()), Times.Once());
+
+            Assert.AreSame(commandMock2.Object, _Queue.Dequeue());
+            Assert.AreSame(commandMock3.Object, _Queue.Dequeue());
+        }
+
+        [TestMethod]
+        public void NoTimeToPlane()
+        {
+            var gameMock = new Mock<IGame>();
+            gameMock.Setup(x => x.DesiredFrameLength).Returns(0);
+            ContextMock.Setup(x => x.HasCommand).Returns(false);
+
+            Behavior.Update(gameMock.Object, AgentMock.Object);
+            PlannerMock.Verify(x => x.PlanBehavior(gameMock.Object, AgentMock.Object), Times.Never());
+        }
     }
 }
