774cb64bb9a95eae1187ec0b0171ba3859b9d80b
DigitalLibrarian <likwid38225@gmail.com>
2016-08-25 21:28:13 -0500

    Removed egregious duplication in body class factory

--- Tiles/DriverConsole/Tiles/Bodies/BodyClassFactory.cs
+++ Tiles/DriverConsole/Tiles/Bodies/BodyClassFactory.cs
@@ -21,72 +21,11 @@ namespace Tiles.Bodies
             });
         }
 
-        public IBodyClass CreateHumanoid()
+        Dictionary<string, BodyPartClass> Human(ITissueClass tissueClass)
         {
-            var tissueClass = CreateTissues();
             var torso = new BodyPartClass("torso", tissueClass, ArmorSlot.Torso, WeaponSlot.None, new List<ICombatMoveClass>());
 
-            var head = new BodyPartClass("head", tissueClass, ArmorSlot.Head, WeaponSlot.None, new List<ICombatMoveClass>(), 
-                parent: torso,
-                canAmputate: true,
-                isCritical: true);
-
-            var leftArm = new BodyPartClass("left arm", tissueClass, ArmorSlot.LeftArm, WeaponSlot.None, new List<ICombatMoveClass>(), 
-                parent: torso,
-                canAmputate: true);
-
-            var leftHand = new BodyPartClass("left hand", tissueClass, ArmorSlot.LeftHand, WeaponSlot.None, new List<ICombatMoveClass>(),
-                parent: leftArm,
-                canGrasp: true,
-                canAmputate: true);
-
-            var rightArm = new BodyPartClass("right arm", tissueClass, ArmorSlot.RightArm, WeaponSlot.None, new List<ICombatMoveClass>(), 
-                parent: torso, 
-                canAmputate: true);
-
-            var rightHand = new BodyPartClass("right hand", tissueClass, ArmorSlot.RightHand, WeaponSlot.Main, new List<ICombatMoveClass>(),
-                parent: rightArm,
-                canGrasp: true,
-                canAmputate: true);
-
-            var leftLeg = new BodyPartClass("left leg", tissueClass, ArmorSlot.LeftLeg, WeaponSlot.None, new List<ICombatMoveClass>(), 
-                parent:torso,
-                canAmputate:true);
-
-            var leftFoot = new BodyPartClass("left foot", tissueClass, ArmorSlot.LeftFoot, WeaponSlot.None, new List<ICombatMoveClass>(),
-                parent: leftLeg,
-                canAmputate: true);
-
-            var rightLeg = new BodyPartClass("right leg", tissueClass, ArmorSlot.RightLeg, WeaponSlot.None, new List<ICombatMoveClass>(), 
-                parent: torso,
-                canAmputate: true);
-
-            var rightFoot = new BodyPartClass("right foot", tissueClass, ArmorSlot.RightFoot, WeaponSlot.None, new List<ICombatMoveClass>(),
-                parent: rightLeg,
-                canAmputate: true);
-
-            var bodyParts = new List<IBodyPartClass>{
-                head,
-                torso,
-                leftArm,
-                rightArm,
-                leftHand,
-                rightHand,
-                leftLeg,
-                rightLeg,
-                leftFoot,
-                rightFoot
-            };
-            return new BodyClass(bodyParts);
-        }
-
-        public IBodyClass CreateFeralHumanoid()
-        {
-            var tissueClass = CreateTissues();
-
-            var torso = new BodyPartClass("torso", tissueClass, ArmorSlot.Torso, WeaponSlot.None, new List<ICombatMoveClass>());
-
-            var head = new BodyPartClass("head", tissueClass, ArmorSlot.Head, WeaponSlot.Teeth, new List<ICombatMoveClass>(),
+            var head = new BodyPartClass("head", tissueClass, ArmorSlot.Head, WeaponSlot.None, new List<ICombatMoveClass>(),
                 parent: torso,
                 canAmputate: true,
                 isCritical: true);
@@ -95,7 +34,7 @@ namespace Tiles.Bodies
                 parent: torso,
                 canAmputate: true);
 
-            var leftHand = new BodyPartClass("left hand", tissueClass, ArmorSlot.LeftHand, WeaponSlot.Claw, new List<ICombatMoveClass>(),
+            var leftHand = new BodyPartClass("left hand", tissueClass, ArmorSlot.LeftHand, WeaponSlot.None, new List<ICombatMoveClass>(),
                 parent: leftArm,
                 canGrasp: true,
                 canAmputate: true);
@@ -104,7 +43,7 @@ namespace Tiles.Bodies
                 parent: torso,
                 canAmputate: true);
 
-            var rightHand = new BodyPartClass("right hand", tissueClass, ArmorSlot.RightHand, WeaponSlot.Claw, new List<ICombatMoveClass>(),
+            var rightHand = new BodyPartClass("right hand", tissueClass, ArmorSlot.RightHand, WeaponSlot.Main, new List<ICombatMoveClass>(),
                 parent: rightArm,
                 canGrasp: true,
                 canAmputate: true);
@@ -125,7 +64,9 @@ namespace Tiles.Bodies
                 parent: rightLeg,
                 canAmputate: true);
 
-            var bodyParts = new List<IBodyPartClass>{
+            var pD = new Dictionary<string, BodyPartClass>();
+
+            foreach(var bc in new List<BodyPartClass>(){
                 head,
                 torso,
                 leftArm,
@@ -136,8 +77,30 @@ namespace Tiles.Bodies
                 rightLeg,
                 leftFoot,
                 rightFoot
-            };
+            })
+            {
+                pD.Add(bc.Name, bc);
+            }
+            return pD;
+        }
+
+
+        public IBodyClass CreateHumanoid()
+        {
+            var tissueClass = CreateTissues();
+            var bodyParts = Human(tissueClass).Values.ToList();
             return new BodyClass(bodyParts);
+        }
+
+        public IBodyClass CreateFeralHumanoid()
+        {
+
+            var tissueClass = CreateTissues();
+            var bodyParts = Human(tissueClass);
+            bodyParts["head"].WeaponSlot = WeaponSlot.Teeth;
+            bodyParts["left hand"].WeaponSlot = WeaponSlot.Claw;
+            bodyParts["right hand"].WeaponSlot = WeaponSlot.Claw;
+            return new BodyClass(bodyParts.Values.ToList());
         }
     }
 }
