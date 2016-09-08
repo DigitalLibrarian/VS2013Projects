5e20ba42cb81208b6ec07a6e2d989deb6ae41744
DigitalLibrarian <likwid38225@gmail.com>
2016-09-08 10:54:05 -0500

    Added guess at momentum calculation. Seems to be working

--- Tiles/DriverConsole/Tiles/Agents/Combat/CombatEvolutions/CombatEvolution_MartialArtsStrike.cs
+++ Tiles/DriverConsole/Tiles/Agents/Combat/CombatEvolutions/CombatEvolution_MartialArtsStrike.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Tiles.Bodies;
 using Tiles.Bodies.Health.Injuries;
+using Tiles.Items;
 using Tiles.Math;
 
 namespace Tiles.Agents.Combat.CombatEvolutions
@@ -42,7 +43,7 @@ namespace Tiles.Agents.Combat.CombatEvolutions
 
             bool isWeaponBased = move.Class.IsItem;
 
-            int momentum = 91;  // TODO - get agent to produce
+            var momentum = GetStrikeMomentum(attacker, move);
 
             bool targetPartWasShed = false;
             IEnumerable<IInjury> injuries = Enumerable.Empty<IInjury>();
@@ -103,6 +104,29 @@ namespace Tiles.Agents.Combat.CombatEvolutions
             if (defenderDies)
             {
                 HandleDeath(attacker, defender, move);
+            }
+        }
+
+        double GetStrikeMomentum(IAgent agent, ICombatMove move)
+        {
+            // M = (Str * VelocityMultiplier) / ((10^6/Size) + ((10 * F) / W)
+
+            if (move.Class.IsItem)
+            {
+                var weapon = move.Weapon;
+                double Str = 1250;
+                double VelocityMultiplier = (double) move.Class.VelocityMultiplier / 1000d;
+                double Size = 60000;
+                double Fat = 1;
+                double W = weapon.GetMass() / 1000d;
+
+                return (Str * VelocityMultiplier)
+                    / ((1000000d / Size) + ((10 * Fat) / W));
+            }
+            else
+            {
+                // TODO 
+                return 50;
             }
         }
     }
