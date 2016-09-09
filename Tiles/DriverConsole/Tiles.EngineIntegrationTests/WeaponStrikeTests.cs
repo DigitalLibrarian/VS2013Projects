42d66db4c2ab029c0bb3aa07e5e95e7e2a105182
DigitalLibrarian <likwid38225@gmail.com>
2016-09-09 13:55:42 -0500

    Added test case for dwarf hacking off a unicorn leg using a steel sword

--- Tiles/DriverConsole/Tiles.EngineIntegrationTests/WeaponStrikeTests.cs
+++ Tiles/DriverConsole/Tiles.EngineIntegrationTests/WeaponStrikeTests.cs
@@ -122,6 +122,65 @@ namespace Tiles.EngineIntegrationTests
             Assert.AreSame(boneLayer, tInjury.Layer);
         }
 
+        [TestMethod]
+        public void DwarfVsUnicorn_ToeWithSteelSwordSlash()
+        {
+            var attacker = GetNewDwarf();
+            var defender = GetNewUnicorn();
+
+            var targetBodyPart = defender.Body.Parts.First(x => x.Name.Equals("right front leg"));
+            Assert.IsNotNull(targetBodyPart);
+
+            var sword = GetInorganicWeapon(DfTags.MiscTags.ITEM_WEAPON_SWORD_SHORT, "STEEL");
+            attacker.Outfit.Wield(sword);
+
+            var slashMoveClass = sword.Class.WeaponClass.AttackMoveClasses.SingleOrDefault(mc => mc.Name.Equals("slash"));
+            Assert.IsNotNull(slashMoveClass);
+
+            var slashMove = CombatMoveBuilder.AttackBodyPartWithWeapon(attacker, defender, slashMoveClass, targetBodyPart, sword);
+
+            var strikeMomentum = attacker.GetStrikeMomentum(slashMove);
+            Assert.AreEqual(914, (int)(strikeMomentum * 10));
+
+            var context = new CombatMoveContext(attacker, defender, slashMove);
+
+            var hairLayer = targetBodyPart.Tissue.TissueLayers.Single(x => x.Material.Name.Equals("hair"));
+            var skinLayer = targetBodyPart.Tissue.TissueLayers.Single(x => x.Material.Name.Equals("skin"));
+            var fatLayer = targetBodyPart.Tissue.TissueLayers.Single(x => x.Material.Name.Equals("fat"));
+            var muscleLayer = targetBodyPart.Tissue.TissueLayers.Single(x => x.Material.Name.Equals("muscle"));
+            var boneLayer = targetBodyPart.Tissue.TissueLayers.Single(x => x.Material.Name.Equals("bone"));
+
+            var injuryReport = InjuryReportCalc.CalculateMaterialStrike(
+                context,
+                slashMoveClass.StressMode,
+                strikeMomentum,
+                slashMoveClass.ContactArea,
+                slashMoveClass.MaxPenetration,
+                targetBodyPart,
+                sword.Class.Material
+                );
+
+            Assert.AreEqual(1, injuryReport.BodyPartInjuries.Count());
+
+            var partInjury = injuryReport.BodyPartInjuries.First();
+            Assert.AreEqual(targetBodyPart, partInjury.BodyPart);
+            Assert.AreSame(BodyPartInjuryClasses.JustTissueDamage, partInjury.Class);
+
+            Assert.AreEqual(3, partInjury.TissueLayerInjuries.Count());
+
+            var tInjury = partInjury.TissueLayerInjuries.ElementAt(0);
+            Assert.AreEqual(TissueLayerInjuryClasses.Tear, tInjury.Class);
+            Assert.AreSame(hairLayer, tInjury.Layer);
+
+            tInjury = partInjury.TissueLayerInjuries.ElementAt(1);
+            Assert.AreEqual(TissueLayerInjuryClasses.Tear, tInjury.Class);
+            Assert.AreSame(skinLayer, tInjury.Layer);
+
+            tInjury = partInjury.TissueLayerInjuries.ElementAt(2);
+            Assert.AreEqual(TissueLayerInjuryClasses.Tear, tInjury.Class);
+            Assert.AreSame(fatLayer, tInjury.Layer);
+
+        }
 
 
 
@@ -129,6 +188,11 @@ namespace Tiles.EngineIntegrationTests
         IAgent GetNewDwarf()
         {
             return DfTagsFascade.CreateCreatureAgent(Atlas, "DWARF", "MALE", Vector3.Zero);
+        }
+
+        IAgent GetNewUnicorn()
+        {
+            return DfTagsFascade.CreateCreatureAgent(Atlas, "UNICORN", "MALE", Vector3.Zero);
         }
 
         IItem GetInorganicWeapon(string name, string materialName)
