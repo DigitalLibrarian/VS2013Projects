9d203f6c0892ecf53ac29e7db5abcd97deecefba
DigitalLibrarian <likwid38225@gmail.com>
2017-05-01 14:40:25 -0500

    Edged_Undefeated_StrainAtYield_To_ResultMomentum

--- Tiles/DriverConsole/Tiles.Materials.Tests/MaterialStrikeResultBuilderTests.cs
+++ Tiles/DriverConsole/Tiles.Materials.Tests/MaterialStrikeResultBuilderTests.cs
@@ -152,16 +152,73 @@ namespace Tiles.Materials.Tests
             Assert.IsTrue(result.ResultMomentum <= momentum, "Conserve energy");
             Assert.AreEqual(momentum, result.ResultMomentum);
         }
+        
+        [TestMethod]
+        public void Edged_Undefeated_StrainAtYield_To_ResultMomentum()
+        {
+            // Stricken strain at yield -> result momentum
+            var d = new Dictionary<int, double>
+            {
+                { 25000, 5d },          // absorb 50%
+                { 50000, 10d },         // no contribution
+                { 100000, 20d },          // yield
+            };
+
+            foreach (var pair in d)
+            {
+                int strainAtYield = pair.Key;
+                double expectedResultMomentum = pair.Value;
+
+                var stressMode = StressMode.Edge;
+                double contactArea = 10d, sharpness = 5000d, momentum = 10d;
+                double thickness = 1d, volume = 1d;
+                var remainingPen = 10d;
+
+                var strikerMaterialMock = new Mock<IMaterial>();
+                var strickenMaterialMock = new Mock<IMaterial>();
+
+                var expectedStress = 10d;
+                var episilon = 0.0001d;
+                StressCalcMock.Setup(x => x.ShearCost1(strikerMaterialMock.Object, strickenMaterialMock.Object, sharpness))
+                    .Returns(expectedStress + episilon);
+
+                int strickenYield = 1, strickenFracture = 1;
+                strickenMaterialMock.Setup(x => x.GetModeProperties(stressMode, out strickenYield, out strickenFracture, out strainAtYield));
+
+                Builder.SetStressMode(stressMode);
+                Builder.SetStrikerContactArea(contactArea);
+                Builder.SetStrikerMaterial(strikerMaterialMock.Object);
+                Builder.SetStrickenContactArea(contactArea);
+                Builder.SetStrickenMaterial(strickenMaterialMock.Object);
+                Builder.SetStrikerSharpness(sharpness);
+                Builder.SetStrikeMomentum(momentum);
+                Builder.SetLayerThickness(thickness);
+                Builder.SetLayerVolume(volume);
+                Builder.SetRemainingPenetration(remainingPen);
+
+                var result = Builder.Build();
+
+                Assert.AreEqual(expectedStress, result.Stress);
+                Assert.AreEqual(momentum, result.Momentum);
+                Assert.AreEqual(contactArea - 1, result.ContactArea);
+                Assert.AreEqual(1d, result.ContactAreaRatio);
+                Assert.AreEqual(StressMode.Edge, result.StressMode);
+                Assert.AreEqual(StressResult.None, result.StressResult);
+                Assert.IsFalse(result.IsDefeated);
+                Assert.AreEqual(expectedResultMomentum, result.ResultMomentum, episilon);
+            }
+        }
 
         [TestMethod]
         public void Edged_Defeated_Sharpness_To_ResultMomentum()
         {
+            // Weapon Sharpness -> result momentum
             var d = new Dictionary<double, double>
             {
-                { 2500d, 8.00006d },    // dull
-                { 5000d, 9.00003d },    // median 
-                { 7500d, 9.33335d },    // sharp
-                { 10000d, 9.500015d },    // double sharp
+                { 2500d, 8.00006d },        // dull
+                { 5000d, 9.00003d },        // median 
+                { 7500d, 9.33335d },        // sharp
+                { 10000d, 9.500015d },      // double sharp
             };
 
             foreach (var pair in d)
