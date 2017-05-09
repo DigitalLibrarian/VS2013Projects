d35436118e17e58446e4aa2991763478ff7c9eeb
DigitalLibrarian <likwid38225@gmail.com>
2017-05-09 14:28:15 -0500

    Blunt_CompleteFracture_TempEdgedConversion()

--- Tiles/DriverConsole/Tiles.Materials.Tests/LayeredMaterialStrikeResultBuilderTests.cs
+++ Tiles/DriverConsole/Tiles.Materials.Tests/LayeredMaterialStrikeResultBuilderTests.cs
@@ -436,20 +436,112 @@ namespace Tiles.Materials.Tests
             AssertTotalLayerTests(2);
         }
 
-        [Ignore]
         [TestMethod]
         public void Blunt_CompleteFracture_TempEdgedConversion()
         {
-            throw new NotImplementedException();
-        }
+            var stressMode = StressMode.Blunt;
+            double momentum = 1d, penetrationLeft = Layer0.Thickness;
 
-        [Ignore]
-        [TestMethod]
-        public void Blunt_UseLastLayerMaterial() 
-        {
-            throw new NotImplementedException();
-        }
+            var strikerMatMock = new Mock<IMaterial>();
+            double strikerContactArea = 1.1d,
+                strikerSharpness = 1.2d;
 
+            double strickenContactArea = 1.3d;
+
+            var layerResult0 = new MaterialStrikeResult
+            {
+                IsDefeated = true,
+                StressResult = StressResult.Impact_CompleteFracture,
+                ResultMomentum = momentum
+            };
+
+            var layerResult1 = new MaterialStrikeResult
+            {
+                IsDefeated = true,
+                ResultMomentum = momentum,
+                StressResult = StressResult.Shear_Cut
+            };
+
+            var layerResult2 = new MaterialStrikeResult
+            {
+                IsDefeated = false,
+                ResultMomentum = momentum,
+                StressResult = StressResult.None
+            };
+            SetupLayerResponse(layerResult0, layerResult1, layerResult2);
+
+            Builder.SetStressMode(stressMode);
+            Builder.SetMomentum(momentum);
+            Builder.SetMaxPenetration(penetrationLeft);
+            Builder.SetStrikerMaterial(strikerMatMock.Object);
+            Builder.SetStrikerContactArea(strikerContactArea);
+            Builder.SetStrikerSharpness(strikerSharpness);
+
+            Builder.SetStrickenContactArea(strickenContactArea);
+
+            Builder.AddLayer(Layer0.Material, Layer0.Thickness, Layer0.Volume, Layer0.Tag);
+            Builder.AddLayer(Layer1.Material, Layer1.Thickness, Layer1.Volume, Layer1.Tag);
+            Builder.AddLayer(Layer2.Material, Layer2.Thickness, Layer2.Volume, Layer2.Tag);
+
+            var result = Builder.Build();
+
+            Assert.AreEqual(Layer1.Thickness, result.Penetration);
+            Assert.AreEqual(3, result.LayerResults.Count());
+            Assert.AreSame(layerResult0, result.LayerResults.ElementAt(0));
+            Assert.AreSame(layerResult1, result.LayerResults.ElementAt(1));
+            Assert.AreSame(layerResult2, result.LayerResults.ElementAt(2));
+            Assert.AreEqual(3, result.TaggedResults.Count());
+            Assert.IsTrue(result.TaggedResults.ContainsKey(Layer0.Tag));
+            Assert.AreEqual(layerResult0, result.TaggedResults[Layer0.Tag]);
+
+            Assert.IsTrue(result.TaggedResults.ContainsKey(Layer1.Tag));
+            Assert.AreEqual(layerResult1, result.TaggedResults[Layer1.Tag]);
+
+            Assert.IsTrue(result.TaggedResults.ContainsKey(Layer2.Tag));
+            Assert.AreEqual(layerResult2, result.TaggedResults[Layer2.Tag]);
+
+            LayerTesterMock.Verify(x => x.StrikeTest(
+                stressMode,
+                strikerMatMock.Object,
+                strikerSharpness,
+                strikerContactArea,
+                momentum,
+                penetrationLeft,
+                Layer0.Material,
+                Layer0.Thickness,
+                Layer0.Volume,
+                strickenContactArea), Times.Once());
+
+            LayerTesterMock.Verify(x => x.StrikeTest(
+                StressMode.Edge,
+                Layer0.Material,
+                strikerSharpness,
+                strikerContactArea,
+                layerResult0.ResultMomentum,
+                penetrationLeft,
+                Layer1.Material,
+                Layer1.Thickness,
+                Layer1.Volume,
+                strickenContactArea
+                ), Times.Once());
+
+
+            LayerTesterMock.Verify(x => x.StrikeTest(
+                StressMode.Blunt,
+                Layer0.Material,
+                strikerSharpness,
+                strikerContactArea,
+                layerResult0.ResultMomentum,
+                penetrationLeft - Layer1.Thickness,
+                Layer2.Material,
+                Layer2.Thickness,
+                Layer2.Volume,
+                strickenContactArea
+                ), Times.Once());
+
+            AssertTotalLayerTests(3);
+        }
+        
         [Ignore]
         [TestMethod]
         public void EndsWithNone()
