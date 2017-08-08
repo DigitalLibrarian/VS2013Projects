8eb59a8fd61f1ceacf8ea319d84f2398ef9e3c81
DigitalLibrarian <likwid38225@gmail.com>
2017-08-08 15:43:32 -0500

    Added dwarf on dwarf wooden pick in leg test

--- Tiles/DriverConsole/Tiles.EngineIntegrationTests/StrikeTests_DwarfVsDwarf.cs
+++ Tiles/DriverConsole/Tiles.EngineIntegrationTests/StrikeTests_DwarfVsDwarf.cs
@@ -1826,6 +1826,46 @@ namespace Tiles.EngineIntegrationTests
             Assert.AreEqual(0d, layerResult.WoundArea);
         }
 
+
+        [TestMethod]
+        public void DwarfVsDwarf_StrikeUpperLegWithWoodPick()
+        {
+            var attacker = Dwarf;
+            var defender = Dwarf;
+
+            var targetBodyPart = defender.Body.Parts.First(x => x.Name.Equals("right upper leg"));
+            Assert.IsNotNull(targetBodyPart);
+
+            var weapon = CreateMaterialTemplateWeapon(DfTags.MiscTags.ITEM_WEAPON_PICK, "WOOD_TEMPLATE");
+            attacker.Outfit.Wield(weapon);
+
+            var moveClass = weapon.Class.WeaponClass.AttackMoveClasses.SingleOrDefault(mc => mc.Name.Equals("strike"));
+
+            var move = CombatMoveBuilder.AttackBodyPartWithWeapon(attacker, defender, moveClass, targetBodyPart, weapon);
+
+            var result = AssertTissueStrikeResults(attacker, defender, targetBodyPart, move,
+                StressResult.Shear_CutThrough,
+                StressResult.Shear_Cut);
+
+            var layerResult = result.BodyPartInjuries.First().TissueLayerInjuries.ElementAt(0);
+            Assert.AreEqual("skin", layerResult.Layer.Name);
+            Assert.AreEqual(1d, layerResult.StrikeResult.PenetrationRatio, 0.01d);
+            Assert.AreEqual(1d, layerResult.StrikeResult.ContactAreaRatio);
+            Assert.AreEqual(0, layerResult.GetDamage().EffectFraction.Numerator);
+            Assert.AreEqual(10000, layerResult.GetDamage().CutFraction.Numerator);
+            Assert.AreEqual(10000, layerResult.GetDamage().DentFraction.Numerator);
+            Assert.AreEqual(layerResult.StrikeResult.ContactArea, layerResult.WoundArea);
+
+            layerResult = result.BodyPartInjuries.First().TissueLayerInjuries.ElementAt(1);
+            Assert.AreEqual("fat", layerResult.Layer.Name);
+            Assert.AreEqual(0.05d, layerResult.StrikeResult.PenetrationRatio, 0.01d);
+            Assert.AreEqual(1d, layerResult.StrikeResult.ContactAreaRatio);
+            Assert.AreEqual(0, layerResult.GetDamage().EffectFraction.Numerator);
+            Assert.AreEqual(430, layerResult.GetDamage().CutFraction.Numerator);
+            Assert.AreEqual(10000, layerResult.GetDamage().DentFraction.Numerator);
+            Assert.AreEqual(0d, layerResult.WoundArea);
+        }
+
         [TestMethod]
         public void DwarfVsDwarf_StrikeRightHandWithSteelPick()
         {
